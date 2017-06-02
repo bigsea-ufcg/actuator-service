25ebd5eed837996a5fc80f176d09f959716a679f
Igor Ataide <igornsa@lsd.ufcg.edu.br>
2017-06-02 15:22:58 -0300

    Fixing bugs on Plugin abstraction

--- plugins/base.py
+++ plugins/base.py
@@ -5,7 +5,7 @@ class Plugin:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def set_cpu_cap(self, vm_id, cap): pass
+    def allocated_resources(self, vm_id): pass
 
     @abstractmethod
     def set_vcpu_cap(self, vm_id, cap): pass
--- plugins/kvm.py
+++ plugins/kvm.py
@@ -6,12 +6,13 @@ import subprocess
 class KVMActuator(Plugin):
 
     def __init__(self):
+        Plugin.__init__(self)
         pass
 
-    def allocated_resources(self, vm_id, cap):
-        command = "virsh schedinfo %s | grep vcpu_quota | awk '{print $3}'" % (vm_id)
-        subprocess.call(command)
+    def allocated_resources(self, vm_id):
+        command = "virsh schedinfo %s | grep vcpu_quota | awk '{print $3}'" % vm_id
+        subprocess.call(command.split())
 
     def set_vcpu_cap(self, vm_id, cap):
         command = "virsh schedinfo %s --set vcpu_quota=%s > /dev/null" % (vm_id, cap * 1000)
-        subprocess.call(command)
+        subprocess.call(command.split())
